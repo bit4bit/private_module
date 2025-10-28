c6602e33bbc168ca76123476e173378990568e9e
Jovany Leandro G.C <bit4bit@riseup.net>
2025-10-29 00:10:39 +0200

    extract compiler state

---
+++ lib/private_module/compiler_state.ex
@@ -0,0 +1,29 @@
+defmodule PrivateModule.CompilerState do
+  use GenServer
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_opts) do
+    :ets.new(__MODULE__, [:set, :public, :named_table])
+    {:ok, %{}}
+  end
+
+  def add_dependency(from, to, ctx) do
+    :ets.insert(__MODULE__, {{from, to, ctx}, to})
+    :ok
+  end
+
+  def select_dependencies(criteria_fun) do
+    Stream.unfold(
+      :ets.first(__MODULE__),
+      fn
+        :"$end_of_table" -> nil
+        key -> {key, :ets.next(__MODULE__, key)}
+      end
+    )
+    |> Stream.filter(criteria_fun)
+  end
+end
--- lib/private_module/mix/tasks/compile/private_module.ex
+++ lib/private_module/mix/tasks/compile/private_module.ex
@@ -2,37 +2,8 @@ defmodule Mix.Tasks.Compile.PrivateModule do
   use Mix.Task.Compiler
   require Mix.Compilers.Elixir
 
-  defmodule CompilerState do
-    use GenServer
+  alias PrivateModule.CompilerState
 
-    def start_link(opts \\ []) do
-      GenServer.start_link(__MODULE__, opts, name: __MODULE__)
-    end
-
-    @impl true
-    def init(_opts) do
-      :ets.new(__MODULE__, [:set, :public, :named_table])
-      {:ok, %{}}
-    end
-
-    def add_dependency(from, to) do
-      :ets.insert(__MODULE__, {{from, to}, to})
-      :ok
-    end
-
-    def select_dependencies(criteria_fun) do
-      Stream.unfold(
-        :ets.first(__MODULE__),
-        fn
-          :"$end_of_table" -> nil
-          key -> {key, :ets.next(__MODULE__, key)}
-        end
-      )
-      |> Stream.filter(criteria_fun)
-    end
-  end
-
-  @recursive true
   @impl Mix.Task.Compiler
   def run(_argv) do
     {:ok, _} = CompilerState.start_link()
@@ -47,7 +18,7 @@ defmodule Mix.Tasks.Compile.PrivateModule do
   def trace({remote, _, to_module, _name, _arity}, env)
       when remote in [:remote_function, :local_function] and
              to_module not in [:elixir_utils, :elixir_module, Module, :elixir_def] do
-    CompilerState.add_dependency(env.module, to_module)
+    CompilerState.add_dependency(env.module, to_module, %{file: env.file, line: env.line})
     :ok
   end
 
@@ -66,7 +37,7 @@ defmodule Mix.Tasks.Compile.PrivateModule do
 
     with {status, diagnostics} when status in [:ok, :noop] <- outcome do
       invalid_dependencies =
-        CompilerState.select_dependencies(fn {source_module, to_module} ->
+        CompilerState.select_dependencies(fn {source_module, to_module, _ctx} ->
           not allowed_to_use_private_module?(source_module, to_module, private_modules)
         end)
 
@@ -109,15 +80,15 @@ defmodule Mix.Tasks.Compile.PrivateModule do
     to_module in private_modules and source_module in private_scopes
   end
 
-  defp diagnostic({source_module, to_module}) do
+  defp diagnostic({source_module, to_module, ctx}) do
     %Mix.Task.Compiler.Diagnostic{
       compiler_name: "private_module",
       details: nil,
-      file: nil,
-      message: "Module #{source_module} is not allowed to call private module #{to_module}",
-      position: 0,
+      file: ctx.file,
+      message:
+        "Module #{source_module} is not allowed to call private module #{to_module}\n at #{ctx.file}:#{ctx.line}",
+      position: ctx.line,
       severity: :error
     }
-    |> struct([])
   end
 end
